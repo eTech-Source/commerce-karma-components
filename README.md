659c795a3669d40578899d2f045bc6af456413d3
eTech-Source <goetch777@gmail.com>
2023-12-16 12:39:50 -0800

    Working on docs and auth

--- README.md
+++ README.md
@@ -1,1 +1,225 @@
-# Test
+# Commerce Karma Components
+
+Commerce Karma components allow you to embed the functionality of Commerce Karma in your application while still being able to add your own custom styling and being able to place them anywhere. These components work on all main stream JavaScript frameworks.
+
+## Installation
+
+### Automatic Installation (Recommended)
+
+Insert a script tag into the `head` or the end of the `body` tag of your HTML markup:
+
+```
+<script src="https://cdn.jsdelivr.net/ADD ASAP"></script>
+```
+
+### Manual Installation
+
+Clone this repository into to the root directory of your project project by running:
+
+```
+git clone https://github.com/eTech-Source/commerce-karma-components.git
+```
+
+Then add the same script tag anywhere you want to use these components:
+
+```
+<script src="/commerce-karma-components/script.js"></script>
+```
+
+**Note:** This approach is not recommended unless you need to modify the source code or change or any of the assets like the stars images. This option is not required to change the styles of the components. However, it does allow for full on premise hosting.
+
+## Usage
+
+Currently nothing will render. This because you have not selected where you want your components to go. Start by placing a `div` tag with an id of the component name you want to render. For example:
+
+```
+<!--Your other HTML markup-->
+<div id="CK-users"></div>
+```
+
+All id's and classes used by the component will need to be prefixed with `CK-`.
+
+Still nothing will appear will appear. Each component has a corresponding function required to render it. For example:
+
+```
+document.addEventListener("DOMContentLoaded", async () => {
+  const users = await injectUser();
+});
+```
+
+It is recommended to wrap the function in a `DOMContentLoaded` event listener.
+
+There is still one more thing to do. Currently the application has not been authorized. Simply embed the sign in components. Into your project by adding a check authentication call. For instance:
+
+```
+document.addEventListener("DOMContentLoaded", async () => {
+  const users = await injectUser();
+  injectSearch ();
+  checkAuth (users)
+});
+```
+
+Note that `users` or review component is required on the same page in order to render the sign in component. All done! You have added your first component to your application.
+
+Here is a short list of all the components:
+
+### Reviews Component
+Renders a review written by a user of Commerce Karma.
+
+#### Basic Usage
+
+Add a `div` tag to your project with an id called `CK-reviews`. 
+
+```
+<div id="CK-reviews"></div>
+```
+
+Then call the `injectReview` function. 
+
+```
+document.addEventListener("DOMContentLoaded", async () => {
+  const reviews = await injectReviews();
+});
+```
+
+#### Function Arguments
+
+This function takes no arguments.
+
+#### Styling
+
+- **ID** `CK-reviews` The parent element for all reviews to be rendered in.
+- **Class** `CK-review` An individual review.
+- **Class** `CK-review-title` The title of a review' child of `CK-review`.
+- **Class** `Ck-review-text` The content of a review; child of `Ck-review`.
+
+
+#### Built in functionality
+
+When the `CK-id` query parameter is present all reviews with that author and all users with that id will be rendered. 
+
+### Search Component
+
+Search Commerce Karma users.
+
+#### Basic Usage
+
+Add a `div` tag to your project with an id called `CK-search`. 
+
+```
+<div id="CK-search"></div>
+```
+
+Then call the `injectSearch` function. 
+
+```
+document.addEventListener("DOMContentLoaded", async () => {
+  const reviews = await injectUsers();
+});
+```
+
+#### Function Arguments
+
+This function takes no arguments.
+
+#### Styling
+
+- **ID** `CK-search` The parent element for the search box.
+- **ID** `CK-search-bar` The input element for the search.
+- **ID** `CK-search-options` Search by category dropdown.
+- **ID** `CK-option` Individual option for search category dropdown; child of `CK-search-options`
+
+#### Built in functionality
+
+On search filter query parameters are added to the url.
+
+### Users Component
+
+Display a Commerce Karma user.
+
+#### Basic Usage
+
+Add a `div` tag to your project with an id called `CK-users`. 
+
+```
+<div id="CK-users"></div>
+```
+
+Then call the `injectUsers` function. 
+
+```
+document.addEventListener("DOMContentLoaded", async () => {
+  const reviews = await injectUsers();
+});
+```
+
+#### Function Arguments
+
+- **Optional** `link` When specified changes the location redirected to. It is recommended to set this to a the route a user profile is on. If this isn't specified the user will be redirected to `/`.
+- **Optional** `filters` Filter user based on a number of properties here a few:
+    - `_id` The mongoDB id of the user.
+    - `userId` The clerk.js user id of the user.
+    - `name` The user's name.
+    - `email` The user's email address.
+    - `phone` The user's phone number.
+    - `customerRating` The user's rating.
+    - `city` The user's city.
+
+   If there are any filters in query params manual passed in filters will override them. Defaults to `{}`.
+
+#### Styling
+
+- **ID** `CK-users` The parent element in which all users are rendered.
+- **Class** `CK-user` A single user; child of `CK-users`
+- **Class** `CK-user-link` A link to the user's profile; child of `CK-user`
+- **Class** `CK-user-name` The user's name; child of `CK-user-link`
+- **Class** `CK-stars` The amount of overall stars the user has; child of `CK-user-link`
+- **Class** `CK-profile-picture` The user's profile picture; child of `CK-user-link`
+
+#### Built in functionality
+
+
+### Sign In Component
+
+If the user is not signed in displays a sign in component.
+
+#### Basic Usage
+
+Add a `div` tag to your project with an id called `CK-signin`. 
+
+```
+<div id="CK-signin"></div>
+```
+
+Then call the `checkAuth` function. 
+
+```
+document.addEventListener("DOMContentLoaded", async () => {
+  const users = await injectUser();
+  checkAuth (users)
+});
+```
+
+#### Function Arguments
+
+**Note:** It is required that one of these arguments is passed.
+
+- `review` The review object from `injectReview`.
+- `user` The user object from `injectUser`.
+
+
+#### Styling
+
+- **ID** `CK-signin` The parent element of which the sign in markup is conditional rendered.
+- **ID** `CK-signin-heading` The "Sign In" heading; child of `CK-signin`
+- **ID** `CK-signin-message` The message telling the user what Commerce Karma is; child of `CK-signin`
+- **ID** `CK-signin-btn` The button for signing in; child of `CK-signin`
+- **ID** `CK-signin-link` The link to the sign in page; child of `CK-signin-btn`
+
+#### Built in functionality
+
+None.
+
+## Usage With Other React.js
+
+This library requires the `document` and `window` API.
--- script.js
+++ script.js
@@ -1,116 +1,311 @@
+// Must be defined first
+
+const injectSignIn = () => {
+  const signInContainer = document.getElementById("CK-signin");
+  signInContainer.innerHTML = `
+        <h1 id="CK-signin-heading">Please Sign In</h1>
+        <p id="CK-signin-message">Sign in to Commerce Karam to rate your customers and view customer ratings.</p>
+        <button id="CK-signin-btn"><a href="http://localhost:3000/sign-in?redrict_url=${window.location.href}" id="CK-signin-link">Sign In</a></button>
+    `;
+};
+
+// Utils
+
+const arrayToKeyValueObject = (arr) => {
+  if (arr.length % 2 !== 0) {
+    throw new Error("Array length must be even for key-value pairing.");
+  }
+
+  const resultObject = {};
+
+  for (let i = 0; i < arr.length; i += 2) {
+    const key = arr[i];
+    const value = arr[i + 1];
+    resultObject[key] = value;
+  }
+
+  return resultObject;
+};
+
+const createStarElement = (src, alt) =>
+  `<img src="${src}" alt="${alt}" height="40" class="star" />`;
+
+const createStars = (count, className) => {
+  const containerElement = document.getElementsByClassName(className);
+  const starsContainer = document.createElement("div");
+  starsContainer.id = "ck-stars";
+
+  if (count < 5) {
+    const remainingCount = 5 - count;
+    if (Number.isInteger(count)) {
+      let starsHtml = "";
+      for (let i = 0; i < count; i++) {
+        starsHtml += createStarElement(
+          "/assets/full-star.png",
+          `${count} stars out of`
+        );
+      }
+      for (let i = 0; i < remainingCount; i++) {
+        starsHtml += createStarElement("/assets/empty-star.png", "5 stars");
+      }
+      starsContainer.innerHTML = starsHtml;
+      return starsContainer;
+    }
+
+    const stars = Math.floor(count);
+    let starsHtml = "";
+    for (let i = 0; i < stars; i++) {
+      starsHtml += createStarElement("/assets/full-star.png", `${stars} and a`);
+    }
+    starsHtml += createStarElement("/assets/half-star.png", "half stars");
+    for (let i = 0; i < remainingCount; i++) {
+      starsHtml += createStarElement("/assets/empty-star.png", "5 stars");
+    }
+    starsContainer.innerHTML += starsHtml;
+    return starsContainer;
+  }
+
+  let starsHtml = "";
+  for (let i = 0; i < 5; i++) {
+    starsHtml += createStarElement("/assets/full-star.png", "5 stars");
+  }
+  starsContainer.innerHTML = starsHtml;
+
+  return starsContainer;
+};
+
+// Get cookie
+
+const readCookie = (name) => {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return "";
+};
+
 // Fetch calls for data
 
 const get = async (id, filters, url) => {
+  const encodedFilters = btoa(JSON.stringify(filters));
 
-    const encodedFilters = btoa (JSON.stringify (filters))
+  // Required to complete any requests other than OPTIONS
 
-    const rawResponse = await fetch (url, {headers: {
-        "id": id,
-        "filters": encodedFilters
-    }})
+  const authorization = readCookie("CK-jwt");
 
-    const response = await rawResponse.json()
-    
-    return response
-}
+  const rawResponse = await fetch(url, {
+    headers: {
+      id: id,
+      filters: encodedFilters,
+      Authorization: authorization,
+    },
+  });
+
+  const response = await rawResponse.json();
+
+  return response;
+};
 
 const post = async (createData, url) => {
-    const rawResponse = await fetch (url, {body: JSON.stringify (createData)})
-    const response = await rawResponse.json()
+  // Required to complete any requests other than OPTIONS
 
-    return response
-}
+  const authorization = readCookie("CK-jwt");
 
+  const rawResponse = await fetch(url, {
+    body: JSON.stringify(createData),
+    method: "POST",
+    headers: { Authorization: authorization },
+  });
+  const response = await rawResponse.json();
+
+  return response;
+};
 
 // Inject data into correct divs
 
 const injectReviews = async () => {
-    const data = await get ("655643cd1c82275babfaf859", {title: "Thank you"}, "http://localhost:3000/api/reviews/add")
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("CK-id");
 
-    const reviews = data.reviews
+  const data = await get(
+    "",
+    id ? { author: id } : null,
+    "http://localhost:3000/api/reviews/add"
+  );
 
-    const reviewsContainer = document.getElementById("CK-reviews")
+  if (data.error) {
+    return false;
+  }
 
-    let injectHtml = ""
+  const reviews = data.reviews;
 
-    for (let i = 0; i < reviews.length; i++) {
-        injectHtml += `
-        
-        <div class="CK-review">
-            <h1 class="CK-review-title">${reviews[i].title}</h1>
-            <b>${reviews[i].stars}</b>
-            <p class="CK-search-text">${reviews[i].text}</p>
-        </div>`
+  const reviewsContainer = document.getElementById("CK-reviews");
+
+  let injectHtml = "";
+
+  for (let i = 0; i < reviews.length; i++) {
+    injectHtml += `
+          
+          <div class="CK-review">
+              <h1 class="CK-review-title">${reviews[i].title}</h1>
+              <b>${reviews[i].stars}</b>
+              <p class="CK-review-text">${reviews[i].text}</p>
+          </div>`;
+  }
+
+  reviewsContainer.innerHTML += injectHtml;
+
+  const stars = createStars(5, "CK-review");
+};
+
+const injectUser = async (link, filters) => {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  if (!filters && urlParams) {
+    for (let params of urlParams) {
+      let paramsArray = [];
+      params.forEach((param) => {
+        paramsArray.push(param.replace("CK-", ""));
+      });
+      filters = arrayToKeyValueObject(paramsArray);
     }
+  }
 
-    reviewsContainer.innerHTML += injectHtml
-}
+  const data = await get(
+    "",
+    filters ? filters : {},
+    "http://localhost:3000/api/user"
+  );
 
-const injectUser = async () => {
-    const data  = await get ("", {}, "http://localhost:3000/api/user")
-    const users = data.user
+  if (data.error) {
+    return false;
+  }
 
-    const usersContainer = document.getElementById("CK-users")
+  const users = data.user;
 
-    let injectHtml = ""
+  const usersContainer = document.getElementById("CK-users");
 
-    for (let i = 0; i < users.length; i++) {
-        injectHtml += `
-        
-        <div class="CK-user">
-            <h1 class="CK-user-name">${users[i].name}</h1>
-        </div>`
-    }
+  let injectHtml = "";
 
-    usersContainer.innerHTML += injectHtml
-}
+  for (let i = 0; i < users.length; i++) {
+    injectHtml += `
+          
+          <div class="CK-user">
+             <a href="${link ? link : `/`}?CK-id=${users[i]._id}" class="CK-user-link">
+                <h1 class="CK-user-name">${users[i].name}</h1>
+                <div class="CK-stars"></div>
+              </a>
+          </div>`;
+  }
+
+  usersContainer.innerHTML = injectHtml;
+};
 
 const injectSearch = async () => {
-    const searchContainer = document.getElementById("CK-search")
-    searchContainer.innerHTML = `
-        <input type="text" placeholder="Search for anyone" id="CK-search-id" class="CK-search"/>
-        <button id="CK-search-btn">Search</button>
-    `
+  const searchContainer = document.getElementById("CK-search");
+  searchContainer.innerHTML = `
+          <input type="text" placeholder="Search for anyone" id="CK-search-bar"/>
+          <select id="CK-search-options" name="search">
+            <option value="name" class="CK-option">By name</option>
+            <option value="email" class="CK-option">By email</option>
+            <option value="phone" class="CK-option">By phone number</option>
+          </select>
+          <button id="CK-search-btn">Search</button>
+      `;
 
-    const searchBox = document.getElementById("CK-search-id")
-    const searchBtn = document.getElementById("CK-search-btn")
+  const searchBox = document.getElementById("CK-search-bar");
+  const searchBtn = document.getElementById("CK-search-btn");
+  const searchOptions = document.getElementById("CK-search-options");
 
-    let users = []
+  searchBtn.onclick = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set(`CK-${searchOptions.value}`, searchBox.value);
+    window.location.search = urlParams;
+  };
+};
 
-    searchBtn.addEventListener ("click", async () => {
-       users =  await populateSearch (searchBox.value)
-       console.log(users)
-    })
+const injectCreateReview = async () => {
+  const createReviewContainer = document.getElementById("CK-create-review");
+  createReviewContainer.innerHTML = `
+    <button id="CK-add-review-btn">Add review</button>
+  `;
+  const createReviewBtn = document.getElementById("CK-add-review-btn");
 
-    const usersContainer = document.getElementById("CK-users")
+  createReviewBtn.addEventListener("click", () => {
+    createReviewContainer.innerHTML = `
+        <form id="CK-create-review-form">
+        <label class="CK-create-review-label">How many stars?</label>
+        <select id="CK-create-review-stars">
+          <option class="CK-create-review-option">1 star</option>
+          <option class="CK-create-review-option">2 stars</option>
+          <option class="CK-create-review-option">3 stars</option>
+          <option class="CK-create-review-option">4 stars</option>
+          <option class="CK-create-review-option" selected>
+            5 stars
+          </option>
+        </select>
+        <br />
+        <label class="CK-create-review-label">What&apos;s your title?</label>
+        <input
+          class="CK-create-review-input"
+          id="CK-create-review-title"
+          name="title"
+          placeholder="Great to work with!"
+        />
+        <br />
+        <label>What do you want to say?</label>
+        <br />
+        <textarea
+          name="review"
+          placeholder="Write your review..."
+          class="CK-create-review-input"
+          id="CK-create-review-text"
+        ></textarea>
+        <br />
+        <button
+         id="CK-create-review-submit"
+          aria-label="Send"
+          type="submit"
+        >
+          Publish
+        </button>
+        </form>
+    `;
+    const publishBtn = document.getElementById("CK-create-review-submit");
 
-    let injectHtml = ""
+    publishBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const stars = document.getElementById("CK-create-review-stars").value;
+      const title = document.getElementById("CK-create-review-title").value;
+      const text = document.getElementById("CK-create-review-text").value;
 
-    for (let i = 0; i < users.length; i++) {
-        injectHtml += `
-        
-        <div class="CK-user">
-            <h1 class="CK-user-name">${users[i].name}</h1>
-        </div>`
-    }
+      console.log(stars, title, text);
+    });
+  });
+};
 
-    usersContainer.innerHTML = injectHtml
-}
+// Authentication: DO NOT MODIFY
 
-
-// Populate some injected items with interactivity
-
-const populateSearch = async (name) => {
-    const data = await get ("", {name: name}, "http://localhost:3000/api/user")
-    return data.user
-}
-
-
-
-//Authentication: DO NOT MODIFY
-const url = window.location.href
-const jwt = url.match (/\[([^)]+)\]/)
+const url = window.location.href;
+const jwt = url.match(/\[(.*?)\]/);
 
 if (jwt) {
-    document.cookie = `CK-jwt=${jwt}; max-age=2592000;`
+  document.cookie = `CK-jwt=${jwt[1]}; max-age=2592000;`;
 }
+
+const checkAuth = async (users, reviews) => {
+  if (users == false || reviews == false) {
+    injectSignIn();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const reviews = await injectReviews();
+  const users = await injectUser();
+  injectCreateReview();
+  injectSearch();
+  await checkAuth(users, reviews);
+});
